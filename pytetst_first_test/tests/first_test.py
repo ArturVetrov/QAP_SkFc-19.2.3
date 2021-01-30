7b089d17706c4f82e5bddb59399bb20445de01c5
Artur <74181556+ArturVetrov@users.noreply.github.com>
2021-01-30 14:04:27 +0300

    Add files via upload

---
+++ pytetst_first_test/tests/first_test.py
@@ -0,0 +1,19 @@
+import pytest
+from app.Calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 5, 2) == 10
+
+    def test_division_calculate_failed(self):
+        assert self.calc.division(self, 50, 5) == 10
+
+    def test_subtraction_calculate_failed(self):
+        assert self.calc.subtraction(self, 20, 10) == 10
+
+    def test_adding_calculate_failed(self):
+        assert self.calc.adding(self, 7, 3) == 10
